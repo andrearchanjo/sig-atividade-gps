261195c7634be645ea16b5fe975008cef0618e71
André Coelho <archanjoandre@yahoo.com.br>
2024-06-05 19:41:52 -0300

    Alterado para trabalhar com layout XML

--- app/src/main/java/com/example/atividade_gps/MainActivity.kt
+++ app/src/main/java/com/example/atividade_gps/MainActivity.kt
@@ -1,46 +1,16 @@
 package com.example.atividade_gps
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.atividade_gps.ui.theme.Atividade_GPSTheme
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            Atividade_GPSTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
+        setContentView(R.layout.activity_main) // Define o layout XML
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Atividade_GPSTheme {
-        Greeting("Android")
+        // Acessa o TextView do layout
+        val textView = findViewById<TextView>(R.id.textView)
+        textView.text = "Hello World!"
     }
 }
